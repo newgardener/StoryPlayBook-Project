7a0a0d5ea0c24b072bc3fa95825e9911bb2ec8f5
JeongwonShin <jennyshin106@gmail.com>
2022-09-12 12:01:57 +0900

    feat: Chip 컴포넌트 생성

---
+++ src/design-system/components/Chip/index.tsx
@@ -0,0 +1,41 @@
+import * as React from "react";
+
+import classNames from "classnames/bind";
+
+import styles from "./styles.module.scss";
+
+const cx = classNames.bind(styles);
+
+export interface ChipProps
+  extends React.PropsWithoutRef<
+    React.DetailedHTMLProps<
+      React.ButtonHTMLAttributes<HTMLButtonElement>,
+      HTMLButtonElement
+    >
+  > {
+  active?: boolean;
+  label?: string | string[];
+  className?: string;
+}
+
+export const Chip = React.forwardRef<HTMLButtonElement, ChipProps>(
+  ({ active = false, disabled = false, label, className, ...restProps }, ref) => {
+    const getText = () => {
+      if (Array.isArray(label) && label.every((v) => typeof v === "string"))
+        return label.join("");
+      if (typeof label === "string") return label;
+      return "";
+    };
+
+    return (
+      <button
+        className={cx("chip", { active, className })}
+        ref={ref}
+        disabled={disabled}
+        {...restProps}
+      >
+        {getText()}
+      </button>
+    );
+  },
+);
