0caa01656dfdbc546bb6376c1fcd9fc2cffe4d70
JeongwonShin <jennyshin106@gmail.com>
2022-09-13 17:00:34 +0900

    feat: Button 컴포넌트에 아이콘도 같이 ButtonText로 사용할 수 있도록 구현
    - icon 위치 결정 가능 (left, right)
    - icon only button도 생성 가능

---
+++ src/design-system/assets/images/index.ts
@@ -0,0 +1,2 @@
+export { ReactComponent as ArrowDown } from "./arrow_down.svg";
+export { ReactComponent as ArrowRight } from "./arrow_right.svg";
--- src/design-system/components/Button/Button.stories.tsx
+++ src/design-system/components/Button/Button.stories.tsx
@@ -1,30 +1,47 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
+import { ArrowRight } from "../../assets/images";
+import { SvgIcon } from "../SvgIcon";
 import { Button } from "./Button";
 import { BUTTON_SIZE } from "./types";
 
 export default {
   title: "디자인시스템/컴포넌트/Button",
   component: Button,
-  args: {
-    children: "신정원버튼테스트",
-  },
 } as Meta<typeof Button>;
 
 export const 버튼_LARGE: StoryObj<typeof Button> = {
   args: {
+    children: "신정원버튼테스트",
     size: BUTTON_SIZE.LARGE,
   },
 };
 
 export const 버튼_MEDIUM: StoryObj<typeof Button> = {
   args: {
+    children: "신정원버튼테스트",
     size: BUTTON_SIZE.MEDIUM,
   },
 };
 
 export const 버튼_SMALL: StoryObj<typeof Button> = {
   args: {
+    children: "신정원버튼테스트",
     size: BUTTON_SIZE.SMALL,
+  },
+};
+
+export const 버튼_ICON_WITH_CHILDREN: StoryObj<typeof Button> = {
+  args: {
+    size: BUTTON_SIZE.MEDIUM,
+    icon: <SvgIcon icon={ArrowRight} />,
+    children: "신정원버튼테스트",
+  },
+};
+
+export const 버튼_ICON_ONLY: StoryObj<typeof Button> = {
+  args: {
+    size: BUTTON_SIZE.MEDIUM,
+    icon: <SvgIcon icon={ArrowRight} />,
   },
 };
