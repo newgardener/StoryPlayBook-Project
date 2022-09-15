efedb3d477bf468e4b9955650855fbc33b3cacfd
JeongwonShin <jennyshin106@gmail.com>
2022-09-16 00:47:11 +0900

    feat: FlexLayout 컴포넌트 생성

---
+++ src/design-system/components/Layout/Layout.stories.tsx
@@ -0,0 +1,9 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { FlexLayout } from "./index";
+
+export default {
+  component: FlexLayout,
+} as Meta<typeof FlexLayout>;
+
+export const 기본: StoryObj<typeof FlexLayout> = {};
