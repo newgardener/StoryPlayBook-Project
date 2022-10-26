a838b1a101fcfa2b4abba487726890c187353e1f
JeongwonShin <jennyshin106@gmail.com>
2022-10-26 21:24:03 +0900

    fix: skeleton toggler, badge 관련 오류 수정

--- src/design-system/components/SkeletonToggler/index.tsx
+++ src/design-system/components/SkeletonToggler/index.tsx
@@ -1,20 +1,24 @@
+import classNames from "classnames/bind";
 import { AnimatePresence, motion } from "framer-motion";
 
 export interface SkeletonTogglerProps {
   showSkeleton?: boolean;
   skeleton?: React.ReactNode;
   children?: React.ReactNode;
+  className?: string;
 }
 
 export const SkeletonToggler = ({
   showSkeleton,
   skeleton,
   children,
+  className,
 }: SkeletonTogglerProps) => {
   return (
     <AnimatePresence>
       <motion.div
         key={showSkeleton ? "skeleton" : "content"}
+        className={classNames(className)}
         initial={{ opacity: 0, position: "relative" }}
         animate={{
           opacity: 1,
